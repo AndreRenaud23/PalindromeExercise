1303a8551146c3659086c4ebb895dbf567e126a2
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-10-04 14:42:02 -0500

    Finished the exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string strInput)
+        {
+
+
+            var length = strInput.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (strInput[i] != strInput[length - i - 1])
+                {
+                    return false;
+                };
+            }
+            return true;
+            
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,24 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("pizza", false)]
+        [InlineData("redivider", true)]
+        [InlineData("raindeer", false)]
+        [InlineData("reviver", true)]
+
+        public void IsAPalindromeTest(string strInput, bool expected)
         {
+            //Arrange
+            var input = new WordSmith();
+
+            //Act
+            var actual = input.IsAPalindrome(strInput);
+
+            //Assert
+            Assert.Equal(actual, expected);
+
 
         }
     }
